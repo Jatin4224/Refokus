17cbb2579be921fd8ac25500d4c6325974a6d8dc
Jatin4224 <jaikumarsharma94130@gmail.com>
2024-12-28 19:42:58 +0530

    adding animation - work component

--- refokuss/src/App.jsx
+++ refokuss/src/App.jsx
@@ -5,8 +5,10 @@ import Navbar from "./components/Navbar";
 import Products from "./components/Products";
 import Stripes from "./components/Stripes";
 import Work from "./components/Work";
+import LocomotiveScroll from "locomotive-scroll";
 
 function App() {
+  const locomotiveScroll = new LocomotiveScroll();
   return (
     <>
       <div className="w-full h-screen bg-zinc-900 text-white font-satoshi ">
--- refokuss/src/components/Work.jsx
+++ refokuss/src/components/Work.jsx
@@ -1,42 +1,78 @@
+import { motion, useScroll, useMotionValueEvent } from "motion/react";
+import { useState } from "react";
+
 const Work = () => {
-  let images = [
+  const [images, setImages] = useState([
     {
       url: "https://cdn.prod.website-files.com/664dc8b6bc52b504509197f0/66a2cefa6def1aabe206b2f6_TTR%20-%2016%209%20(A).webp",
-      top: "50%",
+      top: "60%",
       left: "50%",
-      isActive: true,
+      isActive: false,
     },
     {
       url: "https://cdn.prod.website-files.com/664dc8b6bc52b504509197f0/6697d8c8de92be5a9bdae6f1_Layout%20Land%20-%2016%209%20(A).webp",
-      top: "56%",
-      left: "44%",
+      top: "62%",
+      left: "48%",
       isActive: false,
     },
     {
       url: "https://cdn.prod.website-files.com/664dc8b6bc52b504509197f0/6697d3cddd783017a5e17454_Umlaut%20-%2016%209%20(A).webp",
-      top: "45%%",
-      left: "56%",
+      top: "68%",
+      left: "52%",
       isActive: false,
     },
     {
       url: "https://cdn.prod.website-files.com/664dc8b6bc52b504509197f0/6697d625b55cd38f78a00b3a_Rainfall%20-%2016%209%20(A).webp",
-      top: "60%%",
-      left: "53%",
+      top: "64%",
+      left: "51%",
       isActive: false,
     },
     {
       url: "https://cdn.prod.website-files.com/664dc8b6bc52b504509197f0/6697d625b55cd38f78a00b3a_Rainfall%20-%2016%209%20(A).webp",
-      top: "43%",
-      left: "40%",
+      top: "66%",
+      left: "49%",
       isActive: false,
     },
     {
-      url: "https://cdn.prod.website-files.com/664dc8b6bc52b504509197f0/6697d711ae2b889bd611cdf4_YIR%202021%20-%2016%209%20(A).webphttps://cdn.prod.website-files.com/664dc8b6bc52b504509197f0/6697d625b55cd38f78a00b3a_Rainfall%20-%2016%209%20(A).webp",
-      top: "65 %",
-      left: "55%",
+      url: "https://cdn.prod.website-files.com/664dc8b6bc52b504509197f0/6697d711ae2b889bd611cdf4_YIR%202021%20-%2016%209%20(A).webp",
+      top: "63%",
+      left: "50%",
       isActive: false,
     },
-  ];
+  ]);
+  const { scrollYProgress } = useScroll();
+
+  function imagesShow(arr) {
+    setImages((prev) =>
+      prev.map((item, index) =>
+        arr.indexOf(index) == -1
+          ? { ...item, isActive: false }
+          : { ...item, isActive: true }
+      )
+    );
+  }
+  useMotionValueEvent(scrollYProgress, "change", (latest) => {
+    showAndHideImages(Math.floor(latest * 100));
+  });
+
+  function showAndHideImages(scrollval) {
+    if (scrollval >= 0 && scrollval < 1) {
+      imagesShow([]);
+    } else if (scrollval >= 1 && scrollval < 2) {
+      imagesShow([0]);
+    } else if (scrollval >= 2 && scrollval < 3) {
+      imagesShow([0, 1]);
+    } else if (scrollval >= 3 && scrollval < 4) {
+      imagesShow([0, 1, 2]);
+    } else if (scrollval >= 4 && scrollval < 5) {
+      imagesShow([0, 1, 2, 3]);
+    } else if (scrollval >= 5 && scrollval < 6) {
+      imagesShow([0, 1, 2, 3, 4]);
+      console.log("show 5th image");
+    } else if (scrollval > 6) {
+      imagesShow([0, 1, 2, 3, 4, 5]);
+    }
+  }
   return (
     <div className="w-full mt-10">
       <div className="relative max-w-screen-xl mx-auto text-center">
