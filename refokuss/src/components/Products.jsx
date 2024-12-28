f2ac93b6a4699611840d85b0a9421395533f9243
Jatin4224 <jaikumarsharma94130@gmail.com>
2024-12-28 12:46:25 +0530

    product component

---
+++ refokuss/src/components/Products.jsx
@@ -0,0 +1,42 @@
+import Product from "./Product";
+const Products = () => {
+  let products = [
+    {
+      title: "arqitel",
+      description:
+        "my name is jatin sharma, my fav music artist is avicii,fredagain,dukus,blink-182",
+      live: true,
+      case: false,
+    },
+    {
+      title: "YIR 2022",
+      description:
+        "my name is jatin sharma, my fav music artist is avicii,fredagain,dukus,blink-182",
+      live: true,
+      case: false,
+    },
+    {
+      title: "cisco",
+      description:
+        "my name is jatin sharma, my fav music artist is avicii,fredagain,dukus,blink-182",
+      live: true,
+      case: true,
+    },
+    {
+      title: "openAi",
+      description:
+        "my name is jatin sharma, my fav music artist is avicii,fredagain,dukus,blink-182",
+      live: true,
+      case: true,
+    },
+  ];
+  return (
+    <div className="bg-black">
+      {products.map((val, elem) => (
+        <Product val={val} elem={elem} />
+      ))}
+    </div>
+  );
+};
+
+export default Products;
