4d73dff4f21d5c9f559afdd0fd11d64e82cb4ae3
Jatin4224 <jaikumarsharma94130@gmail.com>
2024-12-29 17:49:56 +0530

    animation product

--- refokuss/src/components/Products.jsx
+++ refokuss/src/components/Products.jsx
@@ -51,18 +51,43 @@ const Products = () => {
           <motion.div
             animate={{ y: -pos + `rem` }}
             transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.5 }}
-            className="w-full h-full bg-sky-100"
-          ></motion.div>
+          >
+            <img
+              src="https://cdn.prod.website-files.com/664dc8b6bc52b504509197f0/6697d711ae2b889bd611cdf4_YIR%202021%20-%2016%209%20(A).webp"
+              alt="Sky Blue Background"
+              className="w-full h-full object-cover"
+            />
+          </motion.div>
           <motion.div
             animate={{ y: -pos + `rem` }}
             transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.5 }}
-            className="w-full h-full bg-sky-500"
-          ></motion.div>
+          >
+            <img
+              src="https://cdn.prod.website-files.com/664dc8b6bc52b504509197f0/6697d8c8de92be5a9bdae6f1_Layout%20Land%20-%2016%209%20(A).webp"
+              alt="Sky Blue Medium"
+              className="w-full h-full object-cover"
+            />
+          </motion.div>
           <motion.div
             animate={{ y: -pos + `rem` }}
             transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.5 }}
-            className="w-full h-full bg-sky-800"
-          ></motion.div>
+          >
+            <img
+              src="https://cdn.prod.website-files.com/664dc8b6bc52b504509197f0/66a2cefa6def1aabe206b2f6_TTR%20-%2016%209%20(A).webp"
+              alt="Sky Blue Dark"
+              className="w-full h-full object-cover"
+            />
+          </motion.div>
+          <motion.div
+            animate={{ y: -pos + `rem` }}
+            transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.5 }}
+          >
+            <img
+              src="https://cdn.prod.website-files.com/664dc8b6bc52b504509197f0/66a2cefa6def1aabe206b2f6_TTR%20-%2016%209%20(A).webp"
+              alt="Sky Blue Dark"
+              className="w-full h-full object-cover"
+            />
+          </motion.div>
         </motion.div>
       </div>
     </div>
